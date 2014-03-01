a6518e272573456908a175b17da7e4097ff79605
joao <joao@192.168.0.130>
2014-03-01 12:43:08 -0300

    exibindo labirinto

--- js/main.js
+++ js/main.js
@@ -1,127 +1,15 @@
 $(document).ready(function() {
 
-
-
-
   // Define o número de salas do labirinto
   var labirinto = new Maze();
   labirinto.width = 4;
   labirinto.height = 3;
 
+  var salasArray = labirinto.create();
 
-  // Cria um array de arrays para conter as salas do labirinto
-  var salasArray = createArray(labirinto.height, labirinto.width);
+  
 
-  // Enche o array de arrays com salas vazias e fechadas
-  for (j = 0; j < labirinto.width; j++) {
-    for (i = 0; i < labirinto.height; i++) {
-      var sala = new Room();
-      sala.hPos = j;
-      sala.vPos = i;
-      salasArray[i][j] = sala;
-
-    }
-  }
-
-  // Escolhe uma sala aleatóriamente para ser a primeira
-  var seed = parseInt(Math.random() * 255);
-  var hPos = parseInt(random(seed) * labirinto.width-1);
-  var vPos = parseInt(random(seed) * labirinto.height-1);
-  var salaAtual = salasArray[vPos][hPos];
-
-
-
-  // Define o número de salas visitadas
-  var salasVisitadas = 0;
-
-  // Uma pilha (LIFO) que guardará as salas já visitadas
-  var pilhaDeSalas = [];
-var i = 0;
-  // Loop que só termina quando todas as salas do labirinto tiverem sido visitadas e configuradas
-  do {
-
-    var salasAdjacentesIntactas = [];
-
-    // Verifica se existe uma sala à leste
-    if (salaAtual.hPos + 1 < labirinto.width) {
-      // Verifica se esta sala está intacta
-      var salaObjLeste = salasArray[salaAtual.vPos][salaAtual.hPos + 1];
-      if (salaObjLeste.walls === null) {
-        // Informa de onde veio o algoritmo até chegar nesta sala
-        salaObjLeste.cameFrom = labirinto.east;
-        // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
-        salasAdjacentesIntactas.push(salaObjLeste);
-      }
-    }
-
-    // Verifica se existe uma sala à sul
-    if (salaAtual.vPos + 1 < labirinto.height) {
-      // Verifica se esta sala está intacta
-      var salaObjSul = salasArray[salaAtual.vPos + 1][salaAtual.hPos];
-      if (salaObjSul.walls === null) {
-        // Informa de onde veio o algoritmo até chegar nesta sala
-        salaObjSul.cameFrom = labirinto.south;
-        // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
-        salasAdjacentesIntactas.push(salaObjSul);
-      }
-    }
-
-    // Verifica se existe uma sala à oeste
-    if (salaAtual.hPos - 1 >= 0) {
-      // Verifica se esta sala está intacta
-      var salaObjOeste = salasArray[salaAtual.vPos][salaAtual.hPos - 1];
-      if (salaObjOeste.walls === null) {
-        // Informa de onde veio o algoritmo até chegar nesta sala
-        salaObjOeste.cameFrom = labirinto.west;
-        // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
-        salasAdjacentesIntactas.push(salaObjOeste);
-      }
-    }
-
-    // Verifica se existe uma sala à norte
-    if (salaAtual.vPos - 1 >= 0) {
-      // Verifica se esta sala está intacta
-      var salaObjNorte = salasArray[salaAtual.vPos - 1][salaAtual.hPos];
-      if (salaObjNorte.walls === null) {
-        // Informa de onde veio o algoritmo até chegar nesta sala
-        salaObjNorte.cameFrom = labirinto.north;
-        // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
-        salasAdjacentesIntactas.push(salaObjNorte);
-      }
-    }
-
-    // Verifica se encontrou alguma sala adjacente intacta
-    if (salasAdjacentesIntactas.length > 0) {
-
-      // Escolhe uma das salas adjacentes aleatóriamente
-      var indice = parseInt(Math.random() * salasAdjacentesIntactas.length);
-      var salaEscolhida = salasAdjacentesIntactas[indice];
-
-      // Coloca a sala atual na pilha de salas
-      pilhaDeSalas.push(salaAtual);
-
-      // Derruba a parede entre a sala atual e a proxima sala
-      // Parede da sala atual
-      salaAtual.walls = salaAtual.walls | salaEscolhida.cameFrom;
-
-      // Parede da sala escolhida (oposta a que foi quebrada na atual)
-      salaEscolhida.walls = salaEscolhida.walls | labirinto.oppositeDirection(salaEscolhida.cameFrom);
-
-      // A célula escolhida será agora a célula atual
-      salaAtual = salaEscolhida;
-
-      // Aumenta 1 ao número de células visitadas
-      salasVisitadas++;
-
-    } else {
-
-      // Se não encontrou uma sala adjacente intacta, pega a última sala salva na pilha e faz o teste com ela
-      salaAtual = pilhaDeSalas.pop();
-
-    }
-  } while (salasVisitadas < ((labirinto.width) * (labirinto.height))-1);
-
-  console.log(salasArray);
+  labirinto.show();
 
 });
 
--- js/maze.js
+++ js/maze.js
@@ -11,7 +11,6 @@ function Maze(_W, _H) {
   this.south = 4;
   this.west = 8;
 
-  this.salasArray = [];
 
 
   this.oppositeDirection = oppositeDirection;
@@ -42,125 +41,156 @@ function Maze(_W, _H) {
     return oppositeDirection;
 
   }
-  
-  
+
+
   this.create = create;
-  function create(){
-// Cria um array de arrays para conter as salas do labirinto
-  var salasArray = createArray(this.height, this.width);
+  function create() {
 
-  // Enche o array de arrays com salas vazias e fechadas
-  for (j = 0; j < this.width; j++) {
-    for (i = 0; i < this.height; i++) {
-      var sala = new Room();
-      sala.hPos = j;
-      sala.vPos = i;
-      salasArray[i][j] = sala;
+    this.salasArray = createArray(this.height, this.width);
 
+    // Enche o array de arrays com salas vazias e fechadas
+    for (j = 0; j < this.width; j++) {
+      for (i = 0; i < this.height; i++) {
+        var sala = new Room();
+        sala.hPos = j;
+        sala.vPos = i;
+        this.salasArray[i][j] = sala;
+
+      }
     }
+
+    // Escolhe uma sala aleatóriamente para ser a primeira
+    var seed = 13;
+    var hPos = parseInt(random(seed) * this.width - 1);
+    var vPos = parseInt(random(seed) * this.height - 1);
+    var salaAtual = this.salasArray[vPos][hPos];
+
+    // Define o número de salas visitadas
+    var salasVisitadas = 0;
+
+    // Uma pilha (LIFO) que guardará as salas já visitadas
+    var pilhaDeSalas = [];
+    var i = 0;
+    // Loop que só termina quando todas as salas do labirinto tiverem sido visitadas e configuradas
+    do {
+
+      var salasAdjacentesIntactas = [];
+
+      // Verifica se existe uma sala à leste
+      if (salaAtual.hPos + 1 < this.width) {
+        // Verifica se esta sala está intacta
+        var salaObjLeste = this.salasArray[salaAtual.vPos][salaAtual.hPos + 1];
+        if (salaObjLeste.walls === null) {
+          // Informa de onde veio o algoritmo até chegar nesta sala
+          salaObjLeste.cameFrom = this.east;
+          // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
+          salasAdjacentesIntactas.push(salaObjLeste);
+        }
+      }
+
+      // Verifica se existe uma sala à sul
+      if (salaAtual.vPos + 1 < this.height) {
+        // Verifica se esta sala está intacta
+        var salaObjSul = this.salasArray[salaAtual.vPos + 1][salaAtual.hPos];
+        if (salaObjSul.walls === null) {
+          // Informa de onde veio o algoritmo até chegar nesta sala
+          salaObjSul.cameFrom = this.south;
+          // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
+          salasAdjacentesIntactas.push(salaObjSul);
+        }
+      }
+
+      // Verifica se existe uma sala à oeste
+      if (salaAtual.hPos - 1 >= 0) {
+        // Verifica se esta sala está intacta
+        var salaObjOeste = this.salasArray[salaAtual.vPos][salaAtual.hPos - 1];
+        if (salaObjOeste.walls === null) {
+          // Informa de onde veio o algoritmo até chegar nesta sala
+          salaObjOeste.cameFrom = this.west;
+          // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
+          salasAdjacentesIntactas.push(salaObjOeste);
+        }
+      }
+
+      // Verifica se existe uma sala à norte
+      if (salaAtual.vPos - 1 >= 0) {
+        // Verifica se esta sala está intacta
+        var salaObjNorte = this.salasArray[salaAtual.vPos - 1][salaAtual.hPos];
+        if (salaObjNorte.walls === null) {
+          // Informa de onde veio o algoritmo até chegar nesta sala
+          salaObjNorte.cameFrom = this.north;
+          // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
+          salasAdjacentesIntactas.push(salaObjNorte);
+        }
+      }
+
+      // Verifica se encontrou alguma sala adjacente intacta
+      if (salasAdjacentesIntactas.length > 0) {
+
+        // Escolhe uma das salas adjacentes aleatóriamente
+        var indice = parseInt(random(seed) * salasAdjacentesIntactas.length);
+        var salaEscolhida = salasAdjacentesIntactas[indice];
+
+        // Coloca a sala atual na pilha de salas
+        pilhaDeSalas.push(salaAtual);
+
+        // Derruba a parede entre a sala atual e a proxima sala
+        // Parede da sala atual
+        salaAtual.walls = salaAtual.walls | salaEscolhida.cameFrom;
+
+        // Parede da sala escolhida (oposta a que foi quebrada na atual)
+        salaEscolhida.walls = salaEscolhida.walls | this.oppositeDirection(salaEscolhida.cameFrom);
+
+        // A célula escolhida será agora a célula atual
+        salaAtual = salaEscolhida;
+
+        // Aumenta 1 ao número de células visitadas
+        salasVisitadas++;
+
+      } else {
+
+        // Se não encontrou uma sala adjacente intacta, pega a última sala salva na pilha e faz o teste com ela
+        salaAtual = pilhaDeSalas.pop();
+
+      }
+    } while (salasVisitadas < ((this.width) * (this.height)) - 1);
+
+    return this.salasArray;
+
   }
 
-  // Escolhe uma sala aleatóriamente para ser a primeira
-  var seed = parseInt(Math.random() * 255);
-  var hPos = parseInt(random(seed) * this.width-1);
-  var vPos = parseInt(random(seed) * this.height-1);
-  var salaAtual = salasArray[vPos][hPos];
+  this.show = show;
+  function show() {
+
+    this.output = '';
+
+    // Enche o array de arrays com salas vazias e fechadas
+    for (j = 0; j < this.height; j++) {
+      for (i = 0; i < this.width; i++) {
+
+        var salaAtual = this.salasArray[j][i];
+        console.log(this.salasArray[j][i]);
+
+        var northWall = salaAtual.walls & this.north ? ' bt' : '';
+        var eastWall = salaAtual.walls & this.east ? ' br' : '';
+        var southWall = salaAtual.walls & this.south ? ' bb' : '';
+        var westWall = salaAtual.walls & this.west ? ' bl' : '';
+
+        var quebraLinha = i===this.width-1?'<div class="clear"></div>':'';
+
+        this.output += '<div class="sala ' + northWall + eastWall + southWall + westWall + ' " ></div>'+quebraLinha;
 
 
-
-  // Define o número de salas visitadas
-  var salasVisitadas = 0;
-
-  // Uma pilha (LIFO) que guardará as salas já visitadas
-  var pilhaDeSalas = [];
-var i = 0;
-  // Loop que só termina quando todas as salas do labirinto tiverem sido visitadas e configuradas
-  do {
-
-    var salasAdjacentesIntactas = [];
-
-    // Verifica se existe uma sala à leste
-    if (salaAtual.hPos + 1 < this.width) {
-      // Verifica se esta sala está intacta
-      var salaObjLeste = salasArray[salaAtual.vPos][salaAtual.hPos + 1];
-      if (salaObjLeste.walls === null) {
-        // Informa de onde veio o algoritmo até chegar nesta sala
-        salaObjLeste.cameFrom = this.east;
-        // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
-        salasAdjacentesIntactas.push(salaObjLeste);
       }
     }
 
-    // Verifica se existe uma sala à sul
-    if (salaAtual.vPos + 1 < this.height) {
-      // Verifica se esta sala está intacta
-      var salaObjSul = salasArray[salaAtual.vPos + 1][salaAtual.hPos];
-      if (salaObjSul.walls === null) {
-        // Informa de onde veio o algoritmo até chegar nesta sala
-        salaObjSul.cameFrom = this.south;
-        // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
-        salasAdjacentesIntactas.push(salaObjSul);
-      }
-    }
+    this.output += '';
 
-    // Verifica se existe uma sala à oeste
-    if (salaAtual.hPos - 1 >= 0) {
-      // Verifica se esta sala está intacta
-      var salaObjOeste = salasArray[salaAtual.vPos][salaAtual.hPos - 1];
-      if (salaObjOeste.walls === null) {
-        // Informa de onde veio o algoritmo até chegar nesta sala
-        salaObjOeste.cameFrom = this.west;
-        // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
-        salasAdjacentesIntactas.push(salaObjOeste);
-      }
-    }
+    console.log(this.output);
 
-    // Verifica se existe uma sala à norte
-    if (salaAtual.vPos - 1 >= 0) {
-      // Verifica se esta sala está intacta
-      var salaObjNorte = salasArray[salaAtual.vPos - 1][salaAtual.hPos];
-      if (salaObjNorte.walls === null) {
-        // Informa de onde veio o algoritmo até chegar nesta sala
-        salaObjNorte.cameFrom = this.north;
-        // Adiciona esta sala a um array de salas adjacentes à atual que estejam intactas
-        salasAdjacentesIntactas.push(salaObjNorte);
-      }
-    }
+    $('body').html(this.output);
 
-    // Verifica se encontrou alguma sala adjacente intacta
-    if (salasAdjacentesIntactas.length > 0) {
-
-      // Escolhe uma das salas adjacentes aleatóriamente
-      var indice = parseInt(Math.random() * salasAdjacentesIntactas.length);
-      var salaEscolhida = salasAdjacentesIntactas[indice];
-
-      // Coloca a sala atual na pilha de salas
-      pilhaDeSalas.push(salaAtual);
-
-      // Derruba a parede entre a sala atual e a proxima sala
-      // Parede da sala atual
-      salaAtual.walls = salaAtual.walls | salaEscolhida.cameFrom;
-
-      // Parede da sala escolhida (oposta a que foi quebrada na atual)
-      salaEscolhida.walls = salaEscolhida.walls | this.oppositeDirection(salaEscolhida.cameFrom);
-
-      // A célula escolhida será agora a célula atual
-      salaAtual = salaEscolhida;
-
-      // Aumenta 1 ao número de células visitadas
-      salasVisitadas++;
-
-    } else {
-
-      // Se não encontrou uma sala adjacente intacta, pega a última sala salva na pilha e faz o teste com ela
-      salaAtual = pilhaDeSalas.pop();
-
-    }
-  } while (salasVisitadas < ((this.width) * (this.height))-1);
-    
-    
-    
   }
-  
+
 
 }
